517c1a31c85f4e19d99fef613f7172be8f55cc0b
Xinyi Chen <xic98@pitt.edu>
2018-03-12 13:42:02 -0400

    Assignment 4-camera & call plugin

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -56,9 +56,30 @@ export class RegisterPage {
       cameraDirection: this.camera.Direction.FRONT
     }
     this.camera.getPicture(options)
-      .then((ImageData) => {
-        this.image = ImageData;
-        console.log(ImageData);
+      .then((imageData) => {
+        this.image = imageData;
+        console.log(imageData);
+      },
+      (err) => {
+        console.log('Error obtaining picture')
+      });
+  }
+
+  getFromLibrary() {
+    const options: CameraOptions = {
+      quality: 100,
+      targetHeight: 100,
+      targetWidth: 100,
+      correctOrientation: true,
+      allowEdit: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      mediaType: this.camera.MediaType.PICTURE,
+    }
+    this.camera.getPicture(options)
+      .then((imageData) => {
+        this.image = imageData;
+        console.log(imageData);
       },
       (err) => {
         console.log('Error obtaining picture')
