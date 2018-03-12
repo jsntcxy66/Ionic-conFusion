d0c724a69f1edce7c1fbc3bf85af96696b829cdc
Xinyi Chen <xic98@pitt.edu>
2018-03-12 00:09:29 -0400

    Social Sharing plugin

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController, ActionSheetController, ModalController } from 'ionic-angular';
+import { SocialSharing } from '@ionic-native/social-sharing';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
@@ -30,14 +31,15 @@ export class DishdetailPage {
     private toastCtrl: ToastController,
     private actionSheetCtrl: ActionSheetController,
     private commentModal: ModalController,
+    private socialSharing: SocialSharing,
     @Inject('BaseURL') private BaseURL) {
 
-      this.dish = navParams.get('dish');
-      this.favorite = favoriteservice.isFavorite(this.dish.id);
-      this.numcomments = this.dish.comments.length;
-      let total = 0;
-      this.dish.comments.forEach(comment => total += comment.rating);
-      this.avgstars = (total/this.numcomments).toFixed(2);
+    this.dish = navParams.get('dish');
+    this.favorite = favoriteservice.isFavorite(this.dish.id);
+    this.numcomments = this.dish.comments.length;
+    let total = 0;
+    this.dish.comments.forEach(comment => total += comment.rating);
+    this.avgstars = (total / this.numcomments).toFixed(2);
   }
 
   ionViewDidLoad() {
@@ -47,7 +49,7 @@ export class DishdetailPage {
   addToFavorites() {
     console.log('Adding to Favorites', this.dish.id);
     this.favorite = this.favoriteservice.addFavorite(this.dish.id);
-    
+
     this.toastCtrl.create({
       message: 'Dish ' + this.dish.id + ' added as favorite successfully',
       position: 'middle',
@@ -57,6 +59,7 @@ export class DishdetailPage {
 
   presentActionSheet() {
     let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select Actions',
       buttons: [
         {
           text: 'Add to Favorites',
@@ -67,13 +70,29 @@ export class DishdetailPage {
         {
           text: 'Add a Comment',
           handler: () => {
-            let modal= this.commentModal.create(CommentPage);
+            let modal = this.commentModal.create(CommentPage);
             modal.present();
             modal.onDidDismiss(comment => {
               if (comment != null)
                 this.dish.comments.push(comment);
               console.log(comment);
             });
+          }
+        },
+        {
+          text: 'Share via Facebook',
+          handler: () => {
+            this.socialSharing.shareViaFacebook(this.dish.name + ' -- ' + this.dish.description, this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Facebook'))
+              .catch(() => console.log('Failed to post to Facebook'));
+          }
+        },
+        {
+          text: 'Share via Twitter',
+          handler: () => {
+            this.socialSharing.shareViaTwitter(this.dish.name + ' -- ' + this.dish.description, this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Twitter'))
+              .catch(() => console.log('Failed to post to Twitter'));
           }
         },
         {
